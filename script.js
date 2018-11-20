c060b3fd35fea7fb7abb035a83a03f9fd0eb96dc
Jose Guerra <jose.guerra@finalsite.com>
2018-11-20 09:25:43 +0000

    created new function rotate object

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
   var headerText = document.querySelector('.site-header .container');
   var bgImage = document.querySelector('.site-header .bgEl');
+  var objectBlock = document.querySelector('.object');
+  var hexObject = document.querySelector('.hexagon');
 
   function setTranslate(xPos, yPos, el) {
     el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
+  }
+
+  function setTranslateRotate(yPos, el) {
+    el.style.transform = "rotate(" + yPos + "deg)"
   }
 
   window.addEventListener("DOMContentLoaded", scrollLoop, false);
@@ -17,6 +23,8 @@ document.addEventListener("DOMContentLoaded", function(){
 
       setTranslate(0, yScrollPosition * -0.4, headerText);
       setTranslate(0, yScrollPosition * 0.7, bgImage);
+      setTranslate(0, yScrollPosition * 0.7, objectBlock);
+      setTranslateRotate(yScrollPosition * 0.9, hexObject);
 
       requestAnimationFrame(scrollLoop);
   }
