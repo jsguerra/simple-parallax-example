ff7b6fc1a0fd67a647e1ba35fc7f8e2926017873
Jose Guerra <jose.guerra@finalsite.com>
2018-11-21 09:07:07 +0000

    updated parallax effects on elements

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ document.addEventListener("DOMContentLoaded", function(){
   var headerText = document.querySelector('.site-header .container');
   var bgImage = document.querySelector('.site-header .bgEl');
   var objectBlock = document.querySelector('.object');
+  var objectBlock2 = document.querySelector('.object2');
+  var objectBlock3 = document.querySelector('.object3');
   var hexObject = document.querySelector('.hexagon');
+  var hexObject2 = document.querySelector('.object2 .hexagon');
+  var hexObject3 = document.querySelector('.object3 .hexagon');
 
   function setTranslate(xPos, yPos, el) {
     el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
@@ -23,8 +27,15 @@ document.addEventListener("DOMContentLoaded", function(){
 
       setTranslate(0, yScrollPosition * -0.4, headerText);
       setTranslate(0, yScrollPosition * 0.7, bgImage);
+
       setTranslate(0, yScrollPosition * 0.7, objectBlock);
       setTranslateRotate(yScrollPosition * 1.2, hexObject);
+
+      setTranslate(0, yScrollPosition * 2, objectBlock2);
+      setTranslateRotate(yScrollPosition * -0.9, hexObject2);
+
+      setTranslate(0, yScrollPosition * 0.9, objectBlock3);
+      setTranslateRotate(yScrollPosition * 1.5, hexObject3);
 
       requestAnimationFrame(scrollLoop);
   }
